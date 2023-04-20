95ee573f059a9b4923297b4112767a4d300094fb
cmcgill436 <candra.mcgill@gmail.com>
2023-04-20 00:00:55 -0500

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,16 @@
 import "animate.css";
 
+import { Link } from "react-router-dom";
+
 export default function Nav(props) {
   return (
-    <div>
-      <header></header>
+    <div className="nav">
+      <Link to="//recipes" className="navLink">
+        <div>Recipes</div>
+      </Link>
+      <Link to="/about" className="navLink">
+        <div>About</div>
+      </Link>
     </div>
   );
 }
--- src/pages/Homepg.js
+++ src/pages/Homepg.js
@@ -5,8 +5,10 @@ import Nav from "../components/Nav";
 export default function Home(props) {
   return (
     <div className="home-body">
+      <header class="animate__animated animate__bounceInDown">
+        Recipe App
+      </header>
       <Nav />
-      <h1 class="animate__animated animate__bounceInDown">Recipe App</h1>
     </div>
   );
 }
